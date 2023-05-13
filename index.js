f709e5706569bda3c301df4a5f98697f42356146
Andres Lagos <lagosfelipe56@gmail.com>
2023-05-13 18:34:41 -0500

    Se agrega evento del boton encriptar, en el que muestra el mensaje encriptado

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
 import { encriptar, desencriptar } from "./encriptacion.js";
 
+const botonEncriptar = document.getElementById("accionEncriptar");
+const botonDesencriptar = document.getElementById("accionDesencriptar")
+// Añadir evento al presionar boton de encriptar
+let textoEncriptado = document.getElementById("encriptar")
+botonEncriptar.addEventListener("click", ()=>{
+    // Obtenemos el valor del texto a encriptar y llamamos la funcion para encriptar
+    
+    // guardamos el texto que hay en el textarea
+    let valorTextoEncriptado = textoEncriptado.value
+    // guardamos la informacion encriptada del textarea
+    let nuevoTexto = encriptar(valorTextoEncriptado)
+    //Obtenemos el id del mensaje interno y el mensaje
+    const mensaje = document.getElementById("mensaje-interno")
+    const mensajeEvento = document.getElementById("mensaje")
+    
+    // Creamos el nodo de texto con el texto encriptado
+    let escribirTexto = document.createTextNode(nuevoTexto)
+    
+    // Creamos un elemento p para luego colocar el nodo de texto con su encriptacion
+    if(mensaje){
+        // Removemos la imagen
+        mensaje.remove()
+        let nuevoParrafo = document.createElement("p")
+        nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
+        // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
+        nuevoParrafo.appendChild(escribirTexto)
+        // Verificar si esta el mensaje en la barra lateral derecha
+        // Creamos un elemento div para colocar el parrafo
+        let nuevoContenedor = document.createElement("div")
+        nuevoContenedor.style.cssText = `
+        width: auto;
+        height: auto;
+        margin: 32px;
+        word-wrap: break-word;
+        display: inline-block;
+        `
+        mensajeEvento.appendChild(nuevoContenedor)
+        nuevoContenedor.id = "nuevoContenedor"
+        let prueba = document.getElementById("nuevoContenedor")
+
+        prueba.appendChild(nuevoParrafo)
+    } else{
+        // Eliminamos el parrafo que creamos para que no
+        // se sobreescriba lo que antes teniamos y luego lo
+        // creamos otra vez
+        document.getElementById("parrafoNuevo").remove()
+        let nuevoParrafo = document.createElement("p")
+        nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
+        // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
+        nuevoParrafo.appendChild(escribirTexto)
+        let prueba = document.getElementById("nuevoContenedor")
+
+        prueba.appendChild(nuevoParrafo)
+     }
+})
