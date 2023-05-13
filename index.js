796c680b2e896285579227f491f0e38203fb0e91
Andres Lagos <lagosfelipe56@gmail.com>
2023-05-13 18:59:01 -0500

    Se añade evento de boton desencriptar, se muestra el mensaje desencriptado

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ botonEncriptar.addEventListener("click", ()=>{
     // Creamos el nodo de texto con el texto encriptado
     let escribirTexto = document.createTextNode(nuevoTexto)
     
-    // Creamos un elemento p para luego colocar el nodo de texto con su encriptacion
+    
     if(mensaje){
         // Removemos la imagen
         mensaje.remove()
+        // Creamos un elemento p para luego colocar el nodo de texto con su encriptacion
         let nuevoParrafo = document.createElement("p")
         nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
         // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
@@ -45,6 +46,58 @@ botonEncriptar.addEventListener("click", ()=>{
         // Eliminamos el parrafo que creamos para que no
         // se sobreescriba lo que antes teniamos y luego lo
         // creamos otra vez
+        document.getElementById("parrafoNuevo").remove()
+        let nuevoParrafo = document.createElement("p")
+        nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
+        // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
+        nuevoParrafo.appendChild(escribirTexto)
+        let prueba = document.getElementById("nuevoContenedor")
+
+        prueba.appendChild(nuevoParrafo)
+     }
+})
+
+botonDesencriptar.addEventListener("click", () =>{
+    // Obtenemos el valor del texto a desencriptar y 
+    //llamamos la funcion para deseencriptar
+    
+    // guardamos el texto que hay en el textarea
+    let valorTextoEncriptado = textoEncriptado.value
+    // guardamos la informacion encriptada del textarea
+    let nuevoTexto = desencriptar(valorTextoEncriptado)
+    //Obtenemos el id del mensaje interno y el mensaje
+    const mensaje = document.getElementById("mensaje-interno")
+    const mensajeEvento = document.getElementById("mensaje")
+    
+    // Creamos el nodo de texto con el texto desencriptado
+    let escribirTexto = document.createTextNode(nuevoTexto)
+    
+    
+    
+    if(mensaje){
+        // Removemos la imagen
+        mensaje.remove()
+        // Creamos un elemento p para luego colocar el nodo de texto con su desencriptacion
+        let nuevoParrafo = document.createElement("p")
+        nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
+        // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
+        nuevoParrafo.appendChild(escribirTexto)
+        // Verificar si esta el mensaje en la barra lateral derecha
+        // Creamos un elemento div para colocar el parrafo
+        let nuevoContenedor = document.createElement("div")
+        nuevoContenedor.style.cssText = `
+        width: auto;
+        height: auto;
+        margin: 32px;
+        word-wrap: break-word;
+        display: inline-block;
+        `
+        mensajeEvento.appendChild(nuevoContenedor)
+        
+        nuevoContenedor.id = "nuevoContenedor"
+        let prueba = document.getElementById("nuevoContenedor")
+        prueba.appendChild(nuevoParrafo)
+    } else{
         document.getElementById("parrafoNuevo").remove()
         let nuevoParrafo = document.createElement("p")
         nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
