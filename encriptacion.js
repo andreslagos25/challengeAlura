029b2fcdc26c58ce77bdbb0a30b119f12bb55f66
Andres Lagos <lagosfelipe56@gmail.com>
2023-05-13 17:35:01 -0500

    Se crea un archivo js para colocar las funciones de encriptación y desencriptacion. Se añade funcion encriptar

---
+++ encriptacion.js
@@ -0,0 +1,36 @@
+export function encriptar(frase){
+    // arreglo que contiene el valor equivalente a cada vocal
+    const vocalesEncript = ["ai","enter","imes","ober","ufat"]
+    // Se hace un ciclo para colocar cada letra en un arreglo
+    let fraseArray = []
+    for (let i = 0; i < frase.length; i++) {
+        fraseArray.push(frase[i]);
+    }
+    /** 
+     * En el for se verifica cada letra para encontrar
+     * una vocal y se remplazara con su equivalente en el
+     * arreglo vocalesEncript
+    */
+    for (let i = 0; i < fraseArray.length; i++) {        
+        if (fraseArray[i] == 'a') {
+            fraseArray.splice(i,1, vocalesEncript[0])
+        }
+        if (fraseArray[i] == 'e') {
+            fraseArray.splice(i,1, vocalesEncript[1])
+        }
+        if (fraseArray[i] == 'i') {
+            fraseArray.splice(i,1, vocalesEncript[2])
+        }
+        if (fraseArray[i] == 'o') {
+            fraseArray.splice(i,1, vocalesEncript[3])
+        }
+        if (fraseArray[i] == 'u') {
+            fraseArray.splice(i,1, vocalesEncript[4])
+        }
+    
+    
+    }
+    // Retornamos la frase encriptada con un join
+    // Para unir todos los elementos en el array
+    return fraseArray.join("")
+}
