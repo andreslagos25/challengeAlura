e10e4023f5d3575547139d94071433d0b12f1877
Andres Lagos <lagosfelipe56@gmail.com>
2023-05-20 18:22:56 -0500

    Se cambio estilos al agregar elementos con js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import { encriptar, desencriptar } from "./encriptacion.js";
 
+// Funcion para copiar
+// function copiar(){
+
+// }
+
 const botonEncriptar = document.getElementById("accionEncriptar");
 const botonDesencriptar = document.getElementById("accionDesencriptar")
+const botonCopiar = document.getElementById("copiar")
 // Añadir evento al presionar boton de encriptar
-let textoEncriptado = document.getElementById("encriptar")
+const textoEncriptado = document.getElementById("encriptar")
+
 botonEncriptar.addEventListener("click", ()=>{
     // Obtenemos el valor del texto a encriptar y llamamos la funcion para encriptar
+    // const divMensaje = document.getElementById("mensaje")
+    const botonCopiar = document.getElementById("copiar")
     
+    botonCopiar.style.display = 'block';
     // guardamos el texto que hay en el textarea
     let valorTextoEncriptado = textoEncriptado.value
     // guardamos la informacion encriptada del textarea
@@ -14,7 +24,8 @@ botonEncriptar.addEventListener("click", ()=>{
     //Obtenemos el id del mensaje interno y el mensaje
     const mensaje = document.getElementById("mensaje-interno")
     const mensajeEvento = document.getElementById("mensaje")
-    
+    mensajeEvento.style.display = 'flex';
+    mensajeEvento.style.flexDirection = 'column-reverse';
     // Creamos el nodo de texto con el texto encriptado
     let escribirTexto = document.createTextNode(nuevoTexto)
     
@@ -23,7 +34,11 @@ botonEncriptar.addEventListener("click", ()=>{
         // Removemos la imagen
         mensaje.remove()
         // Creamos un elemento p para luego colocar el nodo de texto con su encriptacion
-        let nuevoParrafo = document.createElement("p")
+        let nuevoParrafo = document.createElement("p");
+        nuevoParrafo.style.cssText = `
+            word-wrap: break-word;
+            overflow-wrap: break-word;
+        `;
         nuevoParrafo.id = "parrafoNuevo" // Añadimos un ID para luego obtener su valor más fácil
         // Agregamos un hijo con appendchild en este ccaso añadimos el nodo de texto
         nuevoParrafo.appendChild(escribirTexto)
@@ -32,11 +47,11 @@ botonEncriptar.addEventListener("click", ()=>{
         let nuevoContenedor = document.createElement("div")
         nuevoContenedor.style.cssText = `
         width: auto;
-        height: auto;
+        height: 85%;
         margin: 32px;
-        word-wrap: break-word;
-        display: inline-block;
+        display: block;
         `
+        
         mensajeEvento.appendChild(nuevoContenedor)
         nuevoContenedor.id = "nuevoContenedor"
         let prueba = document.getElementById("nuevoContenedor")
